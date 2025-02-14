8876ea38f4dc87138e551b21910d3ceab916f8a1
LuisAlmeidaM <almeidaluis90@gmail.com>
2025-02-13 20:05:34 -0700

    2. Centrado de ventana

--- main.py
+++ main.py
@@ -9,8 +9,23 @@ class Ventana:
         # Establecer el tamaño de la ventana
         self.ventana.geometry(f"{ancho}x{alto}")
 
+        # Centrar la ventana
+        self.CentrarVentana(ancho, alto)
+
         #Llamar al método para iniciar la ventana
         self.Iniciar()
+
+    def CentrarVentana(self, ancho, alto):
+        # Obtener las dimesiones de la ventana
+        anchoPantalla = self.ventana.winfo_screenwidth()
+        altoPantalla = self.ventana.winfo_screenheight()
+
+        #Calcular la posición x Y y de la ventana
+        x = (anchoPantalla // 2) - (ancho // 2)
+        y = (altoPantalla // 2) - (alto // 2)
+
+        # Establecer la posición de la pantalla
+        self.ventana.geometry(f"{ancho}x{alto}+{x}+{y}")
 
     def Iniciar(self):
         self.ventana.mainloop() # Iniciar el bucle principal de la ventana
